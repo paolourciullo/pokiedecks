bef0f15bcfd39c8ece599c38eee478b042b99692
Paolo U <paolourciullo@gmail.com>
2021-07-22 12:08:26 -0400

    add images, card component, tailwind config

---
+++ src/routes/about.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+  <title>About Pokedex Svelte</title>
+</svelte:head>
+
+<h1 class="text-4xl text-center my-8 uppercase">About</h1>
+<p>This is a SvelteKit demo y'all</p>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit yo</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import {pokemon} from '../stores/pokestore';
+  import PokemanCard from '../components/pokemanCard.svelte'
+
+  let searchTerm = "";
+  let filteredPokemon = [];
+
+</script>
+<svelte:head>
+  <title>Pokedex Svelte</title>
+</svelte:head>
+
+<h1 class="text-4xl text-center my-8 uppercase">Welcome to SvelteKit yo</h1>
+
+<input class="w-full rounded-md text-lg p-4 border-2 border-gray-200" type="text" bind:value={searchTerm} placeholder="Search Pokemon">
+
+<div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
+
+  
+  {#each $pokemon as pokeman}
+  <PokemanCard  pokeman={pokeman}/>
+  {/each}
+  
+</div>
+

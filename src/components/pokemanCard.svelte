9440ea1f09cfa8873efd109094a4897fff5e2326
Paolo U <paolourciullo@gmail.com>
2021-07-22 15:22:32 -0400

    build-ready MVP

--- src/components/pokemanCard.svelte
+++ src/components/pokemanCard.svelte
@@ -1,8 +1,9 @@
 <script>
+  import { fade } from 'svelte/transition'
   export let pokeman;
 </script>
 
-<a class='p-6 bg-gray-100 text-gray-800 text-center rounded-md shadow-sm hover:shadow-md flex flex-col items-center' href={`/pokemon/${pokeman.id}`}>
+<a class='p-6 bg-gray-100 text-gray-800 text-center rounded-md shadow-sm hover:shadow-md flex flex-col items-center' href={`/pokemon/${pokeman.id}`} transition:fade>
   <img class="h-40 w-40" src={pokeman.image} alt={pokeman.name}>
   <h2 class="text-2xl uppercase">{pokeman.id}. {pokeman.name}</h2>
 </a>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -38,7 +38,7 @@
   <title>Pokedex Svelte</title>
 </svelte:head>
 
-<h1 class="text-4xl text-center my-8 uppercase">Welcome to SvelteKit yo</h1>
+<h1 class="text-4xl text-center my-8 uppercase">Welcome to Pokey-mania yo</h1>
 
 <input class="w-full rounded-md text-lg p-4 border-2 border-gray-200" type="text" bind:value={searchTerm} placeholder="Search Pokemon">
 
